435ee7a371a7bd62a5327b8c486f22233be34860
Jayasi Mehar <jayasi05@gmail.com>
2013-11-12 03:28:35 +0530

    Making a new image using brightness calculations.

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -20,6 +20,9 @@ namespace WindowsFormsApplication4
     {
         Image<Bgr, Byte> var;
         Image<Gray, Byte> grayvar;
+        double[] brightness = new double[10000];
+        int total;
+        string pathString;
 
         public Form1()
         {
@@ -28,8 +31,8 @@ namespace WindowsFormsApplication4
 
         private void button1_Click(object sender, EventArgs e)
         {
-            int newheight = 50;    //Height and width to which we have to resize. We need to decide how we are finalizing these values.
-            int newwidth = 50;
+            int newheight = 10;    //Height and width to which we have to resize. We need to decide how we are finalizing these values.
+            int newwidth = 10;
             Bitmap bit, resized;    //bit is the original image and resized would be the resized one
             DialogResult result = folderBrowserDialog1.ShowDialog();    //Just like filedialog
             int count = 1;      //Counting number of images that are there in the folder
@@ -38,14 +41,14 @@ namespace WindowsFormsApplication4
             if (result == DialogResult.OK)
             {
                 string[] files = Directory.GetFiles(folderBrowserDialog1.SelectedPath);     //Stores the names of files in the selected folder
-                int total = files.Length;
+                total = files.Length;
                 progressBar1.Maximum = total; //The maximum value of the progress bar is the total number of images that are in the folder. 
-                string pathString = System.IO.Path.Combine(folderBrowserDialog1.SelectedPath, "Mosaic");    //Creating a folder within the given folder by name of Mosaic
+                pathString = System.IO.Path.Combine(folderBrowserDialog1.SelectedPath, "Mosaic");    //Creating a folder within the given folder by name of Mosaic
                 System.IO.Directory.CreateDirectory(pathString);
                 progressBar1.Visible = true;
                 button1.Enabled = false; //Disable buttons while the images are getting chota. 
                 button2.Enabled = false;
-                double[] brightness = new double[total] ;
+               // double[] brightness = new double[total] ;
                 foreach (string fileName in files)  //Picking up each file in the folder
                 {
                     if (ImageExtensions.Contains(Path.GetExtension(fileName).ToUpperInvariant()))   //If the extension is one of those mentioned above
@@ -83,6 +86,9 @@ namespace WindowsFormsApplication4
 
         private void button2_Click(object sender, EventArgs e)
         {
+            int[] match;
+            double min;
+            int index = 1; 
             DialogResult selectresult = openFileDialog1.ShowDialog(); // Show the dialog.
             if (selectresult == DialogResult.OK) // Test result.
             {
@@ -93,20 +99,49 @@ namespace WindowsFormsApplication4
                 imageBox1.Show();
                 Console.Out.Write("The height : " + grayvar.Height);
                 Image<Gray, Byte> Mosaic = new Image<Gray, Byte>(grayvar.Size);
-                Image<Gray, Byte> Mosaic2 = new Image<Gray, Byte>(50,50);
+                grayvar.CopyTo(Mosaic);
+                Image<Gray, Byte> Mosaic2 = new Image<Gray, Byte>(20,20);
                
                 //Processing each 20x20 grid and finding their brightness as well. 
-                for (int i = 0; i < (grayvar.Height); i +=50)
-                for(int j = 0; j<grayvar.Width;j+=50)
+                for (int i = 0; i < (grayvar.Height - 10); i +=10)
+                for(int j = 0; j<grayvar.Width - 10;j+=10)
                 {
                     Rectangle roi = grayvar.ROI;
-                    grayvar.ROI = new Rectangle(j, i, 50, 50);
+                    grayvar.ROI = new Rectangle(j, i, 10, 10);
                     Bitmap smallbox = grayvar.ToBitmap();
-                    double brightnesshere = avgBrightness(smallbox);
-                    Console.Out.Write("Brightness: " + brightnesshere );
-                    grayvar.ROI = roi;
-                }
+                    double brightnesshere = avgBrightness(smallbox); //this is storing the avg brightness of the small grid 
+                    min = 1000;
+                    
+                    for (int k = 0; k < total-2; k++)
+                    {
+                        //Console.Out.Write("Difference is:" + Math.Abs(brightnesshere - brightness[k + 1]) + "at index" + k + " min: " + min);
+                        if (brightnesshere - brightness[k+1] <0 )
+                        {
+                            if (brightness[k + 1] - brightnesshere < min)
+                            {
+                                min = brightness[k + 1] - brightnesshere;
+                                index = k + 1;
+                            }
+                        }
+                        else if (brightnesshere - brightness[k + 1] < min)
+                        {
+                            min = brightnesshere - brightness[k + 1];
+                            index = k + 1;
 
+                        }
+
+                    }
+
+                   Console.Out.Write("Index " + index + "....");
+                    grayvar.ROI = roi;
+                    //index = 9;
+                    if (index == 220)
+                        index = 219;
+                    Image<Gray, Byte> temp = new Image<Gray, Byte>(pathString + "\\" + index.ToString() + ".jpeg");
+                    temp.CopyTo(Mosaic.GetSubRect(new Rectangle(j, i, 10, 10)));
+                }
+               
+               
                 imageBox2.Image = Mosaic;
                 imageBox2.Show();
 
